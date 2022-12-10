baaa37c4e3f613cbded943df5d1c26806eedd51a
kpzip <kpzipgames@gmail.com>
2022-12-09 22:08:38 -0800

    resistor model

--- src/main/java/com/kpzip/circuitsapi/circuitsim/components/IComponent.java
+++ src/main/java/com/kpzip/circuitsapi/circuitsim/components/IComponent.java
@@ -1,6 +1,14 @@
 package com.kpzip.circuitsapi.circuitsim.components;
 
+import com.kpzip.circuitsapi.util.Node;
+
 public interface IComponent {
 	
+	
+	/**
+	 * aV + bI = c
+	 * @return constants a, b, and c in that order
+	 */
+	public double[] getConstraints(Node node1, Node node2);
 	
 }
--- src/main/java/com/kpzip/circuitsapi/circuitsim/components/Resistor.java
+++ src/main/java/com/kpzip/circuitsapi/circuitsim/components/Resistor.java
@@ -1,12 +1,24 @@
 package com.kpzip.circuitsapi.circuitsim.components;
 
+import com.kpzip.circuitsapi.util.Node;
+
 public class Resistor implements IComponent{
 
 	//Resistance in Ohms
 	private double resistance = 0;
 	
-	public Resistor() {
-		// TODO Auto-generated constructor stub
+	private Node n1, n2;
+	
+	public Resistor(Node n1, Node n2) {
+		this.n1 = n1;
+		this.n2 = n2;
 	}
+
+	@Override
+	public double[] getConstraints(Node node1, Node node2) {
+		return new double [] {1.0d, -resistance, 0.0d} ;
+	}
+	
+	
 
 }
